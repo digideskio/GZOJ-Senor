ef893b1d304070c3618bb26e190a7e3805cc1092
zhs490770 <zhs490770@foxmail.com>
2013-08-16 19:50:46 +0800

    最前端的index.php的一点小错误导致节日预览功能无效

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-	if(isset($_SERVER['QUERY_STRING'])&&$_SERVER['QUERY_STRING']!='')
+	if(in_array($_SERVER['QUERY_STRING'],array("0","1","2")))
 	{
 		if(intval($_SERVER['QUERY_STRING'])>=0&&intval($_SERVER['QUERY_STRING'])<=2)
 		header("Location: changeskin.php?id=".$_SERVER['QUERY_STRING']);
