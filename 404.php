5992d07f2b045f799441dfd9ab6ba105190eda34
Sensor <zhs490770@foxmail.com>
2013-09-21 13:54:31 +0800

    更新了404界面

--- 404.php
+++ 404.php
@@ -4,18 +4,19 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="keywords" content="GZOJ,GanZhongOJ,gygjzx,OI,GZOI,赣榆高级中学OI,赣榆高级中学,赣榆高级中学信息学,赣榆高级中学信息学奥林匹克" />
 <title>404 Not Found - GZOJ</title>
-<script language="javascript" src="js_senor/jquery.js"></script>
-<script language="javascript" src="js_senor/jquery.color.js"></script>
-<script language="javascript" src="js_senor/common.js"></script>
-<link rel="stylesheet" type="text/css" href="css_senor/common.css" />
-<link rel="stylesheet" type="text/css" href="css_senor/input.css" />
+<!-- 这个是特殊化处理过的模板，并不适合其他用途！！！！！！！！！！！！！！！！！！！！！  -->
+<script language="javascript" src="/js_senor/jquery.js"></script>
+<script language="javascript" src="/js_senor/jquery.color.js"></script>
+<script language="javascript" src="/js_senor/common.js"></script>
+<link rel="stylesheet" type="text/css" href="/css_senor/common.css" />
+<link rel="stylesheet" type="text/css" href="/css_senor/input.css" />
 <script>$(function(){common_init();});</script>
 </head>
 
 <body>
 <?php require('include/senoroj-navbar.php');?>
 <div id="container">
-<div id="mainboard" class="board" style="height:400px;"><h1>404 Not Found</h1>
+<div id="mainboard" class="board" style="height:400px;"><h1>404 Not Found - <a href="/skin-0">Senor Style</a></h1>
 <div style="width:84%;margin-left:auto;margin-right:auto;font-weight:normal;margin-top:80px;border-left:#9FF 3px solid;min-height:40px;padding-left:20px;background-color:rgba(102,102,102,0.4);color:white;padding-top:15px">页面未找到: <div id="url" style="display:inline"></div></div>
 
 </div>
